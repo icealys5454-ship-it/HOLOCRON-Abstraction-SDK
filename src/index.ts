d3f2ed66b30c4039ace72a011f0f0f81b8aa2c0e
icealys5454-ship-it <icealys5454@gmail.com>
2026-08-11 10:43:26 -0400

    Add index.ts barrel export for all filters and converters

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Filters
+export { Filter } from './abstract/Filter.js';
+export { FilterPipeline } from './filters/FilterPipeline.js';
+export { ScalingFilter } from './filters/ScalingFilter.js';
+export { ColorCorrectionFilter } from './filters/ColorCorrectionFilter.js';
+export { AudioFilter } from './filters/AudioFilter.js';
+
+// Converters
+export { Converter } from './abstract/Converter.js';
+export { ResolutionConverter } from './converters/ResolutionConverter.js';
+export { FrameRateConverter } from './converters/FrameRateConverter.js';
+export { CodecConverter } from './converters/CodecConverter.js';
